041bba6ceb16d618570956c649108a4ba96f9491
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-11 16:25:09 -0400

    Set up themes for the app

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from './providers/next-theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>{children}</ThemeProvider>
         {children}
       </body>
     </html>
