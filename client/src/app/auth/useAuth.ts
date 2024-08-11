17ed5e8f601ea6cb0563d72ceb3c338764273e44
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-11 00:43:17 -0400

    Created useAuth hook

---
+++ client/src/app/auth/useAuth.ts
@@ -0,0 +1,20 @@
+//useAuth hook 
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/firebaseConfig'; 
+
+export const useAuth = () => {
+    const [user, setUser] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return {user, loading};
+}
